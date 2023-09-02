28a646f563b49ceac9fc340c1c78db910bd83649
joycelye <joycelye1991@gmail.com>
2023-09-02 16:16:41 +0800

    init

---
+++ src/main/java/com/llye/externalizedconfig/externalizedconfigyaml/ExternalizedConfigYamlApplication.java
@@ -0,0 +1,13 @@
+package com.llye.externalizedconfig.externalizedconfigyaml;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExternalizedConfigYamlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExternalizedConfigYamlApplication.class, args);
+	}
+
+}
